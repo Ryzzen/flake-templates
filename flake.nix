63d6dc9d10324b8ec31f662bd421694482eec2a5
MAYAUD Antoine <a.mayaud@serma.com>
2024-12-28 17:38:31 +0100

    [REFACTO](General)

--- flake.nix
+++ flake.nix
@@ -7,19 +7,15 @@
       templates = {
         pwn = {
           path = ./pwn;
-          description = "A nix shell for vulnerability research on Linux binaries";
         };
         zephyr-rtos = {
           path = ./zephyr-rtos;
-          description = "Minimal nix shell for zephyr-rtos developpment";
         };
         esp-idf = {
           path = ./esp-idf;
-          description = "Quick and dirty FHS for ESP-IDF developpment";
         };
         c-cpp = {
           path = ./c-cpp;
-          description = "C/C++ Developpment environement";
         };
 
       };
